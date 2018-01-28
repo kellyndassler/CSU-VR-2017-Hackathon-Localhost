7303ea15ad405fdb586e57a9e91d78dfaf430661
Benascor <Peryton272@gmail.com>
2018-01-27 17:28:17 -0700

    Uploading Assets
    Contains our Assets folder

---
+++ Assets/textChanger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using TMPro;
+using System.Collections;
+
+
+
+public class textChanger : MonoBehaviour {
+
+    public TextMeshPro textmeshPro;
+
+    public void changeText(string s)
+    {
+        print(s);
+        textmeshPro = GetComponent<TextMeshPro>();
+        textmeshPro.color = new Color32(0, 255, 0, 255);
+        textmeshPro.SetText(s);
+        textmeshPro.text = (s);
+        textmeshPro.ForceMeshUpdate();
+
+        print(textmeshPro.text);
+    }
+}
